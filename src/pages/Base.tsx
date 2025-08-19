6b69b4ef073a51c4b5b7874c1b510257a4b81609
Erhahon Imafidon <mafiosco4@gmail.com>
2025-08-19 21:09:45 +0100

    enhanced Base component with active state management for base selection

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router';
+import { useState } from 'react';
 
 interface IBaseProps {
     addBase: (base: string) => void;
@@ -8,17 +9,37 @@ interface IBaseProps {
 }
 
 const Base = ({ addBase, pizza }: IBaseProps) => {
+    const [activeBase, setActiveBase] = useState<string | null>(null);
     const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
+    const handleActive = (base: string) => {
+        setActiveBase(base);
+        addBase(base);
+    };
+
     return (
-        <div className="base container">
-            <h3>Step 1: Choose Your Base</h3>
+        <div className="base max-w-75 mt-25 mb-10 mx-auto">
+            <h3 className="pb-2.5 mb-2.5 border-b border-white/20">
+                Step 1: Choose Your Base
+            </h3>
             <ul>
                 {bases.map((base) => {
-                    let spanClass = pizza.base === base ? 'active' : '';
+                    const isActive = activeBase === base;
                     return (
-                        <li key={base} onClick={() => addBase(base)}>
-                            <span className={spanClass}>{base}</span>
+                        <li
+                            className="p-2.5 cursor-pointer"
+                            key={base}
+                            onClick={() => handleActive(base)}
+                        >
+                            <span
+                                className={`${
+                                    isActive
+                                        ? 'font-bold relative before:content-["›"] before:mr-1.5 before:scale-[0.8_1] before:inline-block before:-top-[2px]'
+                                        : ''
+                                }`}
+                            >
+                                {base}
+                            </span>
                         </li>
                     );
                 })}
