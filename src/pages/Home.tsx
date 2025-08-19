72f6220c79e14bfef497c6f62dce147cc43531e4
Erhahon Imafidon <mafiosco4@gmail.com>
2025-08-19 21:34:32 +0100

    refactor Home component for improved styling and layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router';
 
 const Home = () => {
     return (
-        <div className="home container">
-            <h2>Welcome to Pizza Joint</h2>
+        <div className="home max-w-200 mt-25 mb-10 mx-auto text-center">
+            <h2 className="text-[2em] mb-7.5">Welcome to Pizza Joint</h2>
             <Link to="/base">
-                <button>Create Your Pizza</button>
+                <button className="!py-4 !text-[1.6em] !border-3 my-7.5 mx-5">
+                    Create Your Pizza
+                </button>
             </Link>
         </div>
     );
