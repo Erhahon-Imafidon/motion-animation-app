59d6ddbd87028f88acbadbf0e6935386ffeaef7d
Erhahon Imafidon <mafiosco4@gmail.com>
2025-08-19 12:49:33 +0100

    added the last page component

---
+++ src/pages/Toppings.tsx
@@ -0,0 +1,47 @@
+// src/pages/Toppings.tsx
+import { Link } from 'react-router-dom';
+
+type Pizza = {
+    base: string;
+    toppings: string[];
+};
+
+type ToppingsProps = {
+    pizza: Pizza;
+    addTopping: (topping: string) => void;
+};
+
+const Toppings: React.FC<ToppingsProps> = ({ addTopping, pizza }) => {
+    const toppings = [
+        'mushrooms',
+        'peppers',
+        'onions',
+        'olives',
+        'extra cheese',
+        'tomatoes',
+    ];
+
+    return (
+        <div className="toppings container">
+            <h3>Step 2: Choose Toppings</h3>
+            <ul>
+                {toppings.map((topping) => {
+                    const spanClass = pizza.toppings.includes(topping)
+                        ? 'active'
+                        : '';
+                    return (
+                        <li key={topping} onClick={() => addTopping(topping)}>
+                            <span className={spanClass}>{topping}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+
+            <Link to="/order">
+                <button>Order</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Toppings;
