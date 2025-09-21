3d3b2cdb48a153ef34a8bd73abea3d4c00061b4e
Erhahon Imafidon <mafiosco4@gmail.com>
2025-09-21 02:18:58 +0100

    refactor Layout and App components to include Header and streamline structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home.tsx';
 import Toppings from './pages/Toppings.tsx';
 import Layout from './components/Layout.tsx';
 import Order from './pages/Order.tsx';
+import Header from './components/Header.tsx';
 
 type Pizza = {
     base: string;
@@ -31,24 +32,37 @@ const App = () => {
     };
 
     return (
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route
-                        path="base"
-                        element={<Base addBase={addBase} pizza={pizza} />}
-                    />
-                    <Route
-                        path="toppings"
-                        element={
-                            <Toppings addTopping={addTopping} pizza={pizza} />
-                        }
-                    />
-                    <Route path="order" element={<Order pizza={pizza} />} />
-                </Route>
-            </Routes>
-        </AnimatePresence>
+        <div className="min-h-screen">
+            <Header />
+            <main className="text-white relative overflow-hidden">
+                <AnimatePresence mode="wait">
+                    <Routes location={location} key={location.pathname}>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Home />} />
+                            <Route
+                                path="base"
+                                element={
+                                    <Base addBase={addBase} pizza={pizza} />
+                                }
+                            />
+                            <Route
+                                path="toppings"
+                                element={
+                                    <Toppings
+                                        addTopping={addTopping}
+                                        pizza={pizza}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="order"
+                                element={<Order pizza={pizza} />}
+                            />
+                        </Route>
+                    </Routes>
+                </AnimatePresence>
+            </main>
+        </div>
     );
 };
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,6 @@
 import { Outlet } from 'react-router';
-import Header from './Header.tsx';
 
 const Layout = () => {
-    return (
-        <div className="min-h-screen">
-            <Header />
-            <main className="text-white relative overflow-hidden">
-                <Outlet />
-            </main>
-        </div>
-    );
+    return <Outlet />;
 };
 export default Layout;
